5420c1a6b8b73259ac3eb5b0cef9460be3166087
Gabo <gleyvamerino@smu.edu>
2017-09-26 17:46:14 -0500

    First commit

--- Lab3/GameScene.swift
+++ Lab3/GameScene.swift
@@ -8,82 +8,72 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var spaceship : SKSpriteNode?
+    private var motionManager: CMMotionManager?
+    var destX:CGFloat?
     
     override func didMove(to view: SKView) {
+        prepareMotionManager()
+        createSpaceship()
+        createBackground()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //set screen bounds
+        self.view?.bounds = CGRect(x: 0, y: 0, width: (self.view?.frame.size.width)!, height: (self.view?.frame.size.height)!)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+    }
+    
+    fileprivate func prepareMotionManager() {
+        motionManager = CMMotionManager()
+        destX = 0.0
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        if motionManager?.isAccelerometerAvailable == true {
+            // 2
+            motionManager?.startAccelerometerUpdates(to: OperationQueue.current!, withHandler:{
+                data, error in
+                
+                let currentX = self.spaceship?.position.x
+                
+                // 3
+                if (data?.acceleration.x)! < 0.0 {
+                    self.destX = currentX! + CGFloat((data?.acceleration.x)! * 100)
+                }
+                    
+                else if (data?.acceleration.x)! > 0.0 {
+                    self.destX = currentX! + CGFloat((data?.acceleration.x)! * 100)
+                }
+                
+            })
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    fileprivate func createSpaceship() {
+        let spaceTexture = SKTexture(image: #imageLiteral(resourceName: "Spaceship"))
+        let node = SKSpriteNode(texture: spaceTexture)
+        node.size = CGSize(width: 200, height: 200)
+        node.position = CGPoint(x: ((self.view?.frame.size.width)! / 2) - 200, y: -(self.view?.frame.size.height)! + 225)
+        node.zPosition = 1
+        self.spaceship = node
+        self.addChild(self.spaceship!)
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    fileprivate func createBackground() {
+        let spaceTexture = SKTexture(image: #imageLiteral(resourceName: "SpaceBackground"))
+        let node = SKSpriteNode(texture: spaceTexture)
+        node.size = CGSize(width: frame.size.width, height: frame.size.height)
+        node.position = CGPoint(x: 0, y: 0)
+        node.zPosition = 0
+        self.addChild(node)
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        let action = SKAction.moveTo(x: destX!, duration: 1)
+        self.spaceship?.run(action)
     }
 }
