9b9f238341807d3c751653650d18c8d7f593628b
Gabo <gleyvamerino@smu.edu>
2017-09-26 23:02:46 -0500

    background Animation added

--- Lab3/GameScene.swift
+++ Lab3/GameScene.swift
@@ -14,12 +14,15 @@ class GameScene: SKScene {
     
     private var spaceship : SKSpriteNode?
     private var motionManager: CMMotionManager?
+    private var bg1: SKSpriteNode?
+    private var bg2: SKSpriteNode?
     var destX:CGFloat?
     
     override func didMove(to view: SKView) {
         prepareMotionManager()
         createSpaceship()
         createBackground()
+        createMeteor()
         
         //set screen bounds
         self.view?.bounds = CGRect(x: 0, y: 0, width: (self.view?.frame.size.width)!, height: (self.view?.frame.size.height)!)
@@ -39,15 +42,22 @@ class GameScene: SKScene {
                 
                 // 3
                 if (data?.acceleration.x)! < 0.0 {
-                    self.destX = currentX! + CGFloat((data?.acceleration.x)! * 100)
+                    self.destX = currentX! + CGFloat((data?.acceleration.x)! * 900)
                 }
                     
                 else if (data?.acceleration.x)! > 0.0 {
-                    self.destX = currentX! + CGFloat((data?.acceleration.x)! * 100)
+                    self.destX = currentX! + CGFloat((data?.acceleration.x)! * 900)
                 }
                 
             })
             
+        }
+    }
+    
+    fileprivate func createMeteor() {
+        if let particles = SKEmitterNode(fileNamed: "meteor.sks") {
+            particles.position = CGPoint(x: 150, y: 250)
+            addChild(particles)
         }
     }
     
@@ -63,17 +73,41 @@ class GameScene: SKScene {
     
     fileprivate func createBackground() {
         let spaceTexture = SKTexture(image: #imageLiteral(resourceName: "SpaceBackground"))
+        
         let node = SKSpriteNode(texture: spaceTexture)
         node.size = CGSize(width: frame.size.width, height: frame.size.height)
         node.position = CGPoint(x: 0, y: 0)
         node.zPosition = 0
-        self.addChild(node)
+        self.bg1 = node
+        self.addChild(self.bg1!)
+        
+        let node2 = SKSpriteNode(texture: spaceTexture)
+        node2.size = CGSize(width: frame.size.width, height: frame.size.height)
+        node2.position = CGPoint(x: 0, y: (self.bg1?.size.height)! - 1)
+        node2.zPosition = 0
+        self.bg2 = node2
+        self.addChild(self.bg2!)
+    }
+    
+    fileprivate func animateBackground() {
+        self.bg1?.position = CGPoint(x: (self.bg1?.position.x)!, y: (self.bg1?.position.y)! - 4)
+        self.bg2?.position = CGPoint(x: (self.bg2?.position.x)!, y: (self.bg2?.position.y)! - 4)
+        
+        if (self.bg1?.position.y)! < -(self.bg1?.size.height)! {
+            self.bg1?.position = CGPoint(x: (self.bg1?.position.x)!, y: (self.bg2?.position.y)! + (self.bg2?.size.height)!)
+        }
+        
+        if (self.bg2?.position.y)! < -(self.bg2?.size.height)! {
+            self.bg2?.position = CGPoint(x: (self.bg2?.position.x)!, y: (self.bg1?.position.y)! + (self.bg1?.size.height)!)
+        }
     }
     
     
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         let action = SKAction.moveTo(x: destX!, duration: 1)
         self.spaceship?.run(action)
+        self.animateBackground()
     }
 }
