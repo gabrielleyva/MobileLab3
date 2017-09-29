78cae58633bf58644663954337abc646a854a22d
Gabo <gleyvamerino@smu.edu>
2017-09-29 12:28:51 -0500

    Game Basically Done

--- Lab3/GameScene.swift
+++ Lab3/GameScene.swift
@@ -10,27 +10,38 @@ import SpriteKit
 import GameplayKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    //Game Coliision categories
+    let meteorCategory: UInt32 = 0x1 << 1
+    let spaceshipCategory: UInt32 = 0x1 << 2
+    
+    //Game Nodes
     private var spaceship : SKSpriteNode?
     private var motionManager: CMMotionManager?
     private var bg1: SKSpriteNode?
     private var bg2: SKSpriteNode?
-    private var meteors = [SKEmitterNode]()
+    private var label = SKLabelNode()
+    private var bigLabel = SKLabelNode()
+    
+    //Game Variables
     var destX:CGFloat?
     var timer = Timer()
+    var score = 0
+    var hit = false
+    var start = false
     
     override func didMove(to view: SKView) {
-        prepareMotionManager()
-        createSpaceship()
         createBackground()
         createBoundries()
-        scheduleMeteorShower()
+        prepareLabel()
+        prepareBigLabel(text: "Tap To Start")
         
         //set screen bounds
         self.view?.bounds = CGRect(x: 0, y: 0, width: (self.view?.frame.size.width)!, height: (self.view?.frame.size.height)!)
         
         self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -4.0)
+        self.physicsWorld.contactDelegate = self
         
     }
     
@@ -38,6 +49,8 @@ class GameScene: SKScene {
         let bottomOffset = CGFloat(400.0);
         let newFrame = CGRect(x: 0.0, y: -bottomOffset, width: self.frame.size.width, height: self.frame.size.height + bottomOffset)
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: newFrame)
+        self.physicsBody?.affectedByGravity = false
+        self.physicsBody?.isDynamic = true
     }
     
     fileprivate func prepareMotionManager() {
@@ -45,13 +58,10 @@ class GameScene: SKScene {
         destX = 0.0
         
         if motionManager?.isAccelerometerAvailable == true {
-            // 2
             motionManager?.startAccelerometerUpdates(to: OperationQueue.current!, withHandler:{
                 data, error in
                 
                 let currentX = self.spaceship?.position.x
-                
-                // 3
                 if (data?.acceleration.x)! < 0.0 {
                     self.destX = currentX! + CGFloat((data?.acceleration.x)! * 900)
                 }
@@ -65,11 +75,36 @@ class GameScene: SKScene {
         }
     }
     
+    fileprivate func prepareBigLabel(text: String) {
+        self.bigLabel = SKLabelNode(fontNamed: "Avenir-BlackOblique")
+        self.bigLabel.color = .white
+        self.bigLabel.text = text
+        self.bigLabel.position = CGPoint(x: ((self.view?.frame.size.width)! / 2) - (self.label.frame.size.width), y: (self.view?.frame.size.height)! / 2 - (self.label.frame.size.height))
+        self.bigLabel.zPosition = 1
+        self.bigLabel.numberOfLines = 2
+        self.bigLabel.lineBreakMode = .byWordWrapping
+        self.bigLabel.fontSize = CGFloat(72)
+        self.addChild(self.bigLabel)
+    }
+    
+    fileprivate func prepareLabel() {
+        self.label = SKLabelNode(fontNamed: "Avenir-BlackOblique")
+        self.label.color = .white
+        self.label.text = "Score: 0"
+        self.label.position = CGPoint(x: (self.view?.frame.size.width)! - 120, y: (self.view?.frame.size.height)! - 55)
+        self.label.zPosition = 1
+        self.label.fontSize = CGFloat(42)
+        self.addChild(self.label)
+    }
+    
     fileprivate func createMeteor() -> SKEmitterNode{
         if let particle = SKEmitterNode(fileNamed: "meteor.sks") {
             particle.position = CGPoint(x: (self.spaceship?.position.x)!, y: frame.size.height)
-            particle.physicsBody = SKPhysicsBody(circleOfRadius: 25)
-            
+            particle.physicsBody = SKPhysicsBody(circleOfRadius: 5)
+            particle.physicsBody?.usesPreciseCollisionDetection = true
+            particle.physicsBody?.categoryBitMask = meteorCategory
+            particle.physicsBody?.collisionBitMask = 0
+
             return particle
         }
         
@@ -82,6 +117,15 @@ class GameScene: SKScene {
         node.size = CGSize(width: 200, height: 200)
         node.position = CGPoint(x: ((self.view?.frame.size.width)! / 2) - 200, y: -(self.view?.frame.size.height)! + 225)
         node.zPosition = 1
+        
+        node.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 100))
+        node.physicsBody?.affectedByGravity = false
+        node.physicsBody?.isDynamic = true
+        node.physicsBody?.usesPreciseCollisionDetection = true
+        node.physicsBody?.categoryBitMask = spaceshipCategory
+        node.physicsBody?.contactTestBitMask = meteorCategory
+        node.physicsBody?.allowsRotation = false
+        
         self.spaceship = node
         self.addChild(self.spaceship!)
     }
@@ -118,20 +162,73 @@ class GameScene: SKScene {
     }
     
     fileprivate func scheduleMeteorShower(){
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.generateMeteors), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.8, target: self, selector: #selector(self.generateMeteors), userInfo: nil, repeats: true)
     }
     
     @objc fileprivate func generateMeteors() {
-        let node = self.createMeteor()
-        self.addChild(node)
-        self.meteors.append(node)
+        
+        if hit == false {
+            let node = self.createMeteor()
+            self.addChild(node)
+        
+            let wait = SKAction.wait(forDuration: 3)
+            let complete = SKAction.run {
+                node.removeFromParent()
+                self.score = self.score + 1
+                self.label.text = "Score: " + String(self.score)
+            }
+            
+            node.run(SKAction.sequence([wait, complete]))
+        }
+        
     }
-    
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        let action = SKAction.moveTo(x: destX!, duration: 0.8)
-        self.spaceship?.run(action)
-        self.animateBackground()
+        if start == true {
+            let action = SKAction.moveTo(x: destX!, duration: 0.8)
+            self.spaceship?.run(action)
+            self.animateBackground()
+        }
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if start == false {
+            start = true
+            hit = false
+            self.bigLabel.removeFromParent()
+            createSpaceship()
+            prepareMotionManager()
+            scheduleMeteorShower()
+        }
+    }
+    
+    //MARK CONTACT DELEGATE METHODS
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        print("Contact")
+        
+        var firstBody = SKPhysicsBody()
+        var secondBody = SKPhysicsBody()
+        // 2 Assign the two physics bodies so that the one with the lower category is always stored in firstBody
+        if  contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        
+        if firstBody.categoryBitMask == meteorCategory && secondBody.categoryBitMask == spaceshipCategory {
+            start = false
+            hit = true
+            score = 0
+            self.timer.invalidate()
+            self.spaceship?.removeFromParent()
+            self.prepareBigLabel(text: " Game Over!\nTap To Restart")
+        }
+
+    }
+
 }
